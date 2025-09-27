664f6c660677d00b8b55febef9e7102828a5f270
adisa39 <yusufhammed39@gmail.com>
2025-09-27 19:10:49 +0100

    rename db url to uri

--- src/config/dbConnection.ts
+++ src/config/dbConnection.ts
@@ -7,9 +7,9 @@ export const connectDB = async () => {
   try {
     // Only log the MongoDB URL in non-production environments
     if (env.NODE_ENV !== 'production') {
-      logger.info(`Connecting to MongoDB with URL: ${env.MONGODB_URL}`);
+      logger.info(`Connecting to MongoDB with URL: ${env.MONGODB_URI}`);
     }
-    await mongoose.connect(env.MONGODB_URL);
+    await mongoose.connect(env.MONGODB_URI);
     logger.info("Successful connection to MongoDB.");
   } catch (error) {
     logger.error('Failed connection to MongoDB:', error);
