4c6086588368d69a40d7b6d9aeb447873b782322
adisa39 <yusufhammed39@gmail.com>
2025-09-28 12:38:27 +0100

    attempt to fix auth api endpoint error in production

--- src/helpers/jwt.ts
+++ src/helpers/jwt.ts
@@ -3,41 +3,40 @@ import jwt from "jsonwebtoken";
 import { IUser } from "../types";
 import User from "../models/user";
 import { env } from "../utils/env";
-
-// import console from '../config/loggingConfig';
+import logger from "../config/loggingConfig";
 
 export const generateUserToken = (user: IUser) => {
   try {
-    console.info(`Generating token for user: ${user.username}`);
-    const token = jwt.sign({ username: user.username, _id: user._id, password: user.pi_uid }, env.JWT_SECRET, {
+    logger.info(`Generating token for user: ${user.username}`);
+    const token = jwt.sign({ username: user.username, _id: user._id, userId: user.pi_uid }, env.JWT_SECRET, {
       expiresIn: "1d", // 1 day
     });
-    console.info(`Successfully generated token for user: ${user.username}`);
+    logger.info(`Successfully generated token for user: ${user.username}`);
     return token;
   } catch (error) {
-    console.error(`Failed to generate user token for piUID ${ user.username }:`, error);
+    logger.error(`Failed to generate user token for piUID ${ user.username }:`, error);
     throw new Error('Failed to generate user token; please try again');
   }
 };
 
 export const decodeUserToken = async (token: string) => {
   try {
-    console.info(`Decoding token.`);
+    logger.info(`Decoding token.`);
     const decoded = jwt.verify(token, env.JWT_SECRET) as { _id: string };
     if (!decoded._id) {
-      console.warn(`Invalid token: Missing userID.`);
+      logger.warn(`Invalid token: Missing userID.`);
       throw new Error("Invalid token: Missing userID.");
     }
-    console.info(`Finding user associated with token: ${decoded._id}`);
+    logger.info(`Finding user associated with token: ${decoded._id}`);
     const associatedUser = await User.findById(decoded._id);
     if (!associatedUser) {
-      console.warn(`User not found for token: ${decoded._id}`);
+      logger.warn(`User not found for token: ${decoded._id}`);
       throw new Error("User not found.");
     }
-    console.info(`Successfully decoded token and found user: ${associatedUser._id}`);
+    logger.info(`Successfully decoded token and found user: ${associatedUser._id}`);
     return associatedUser;
   } catch (error) {
-    console.error('Failed to decode user token:', error);
+    logger.error('Failed to decode user token:', error);
     throw new Error('Failed to decode user token; please try again');
   }
 };
